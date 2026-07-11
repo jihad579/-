533bdcf49fc626551a7110203208b7849fd97968
jihad579 <mz3037047@gmail.com>
2026-07-11 17:14:57 +0600

    Update cmd.js

--- Script/commands/cmd.js
+++ Script/commands/cmd.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "cmd",
     version: "1.0.0",
     hasPermssion: 2,
-    credits: "MAHBUB SHAON",
+    credits: "Ak jihad",
     description: "Manage/Control all bot modules",
     commandCategory: "System",
     usages: "[load/unload/loadAll/unloadAll/info] [name module]",
